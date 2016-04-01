afedbe374c3acdba2e55bfe2f99509bce36a9b8a
t2n3qqq <t2n3qqq@gmail.com>
2016-04-01 19:25:56 +0300

    Create Main Activity

---
+++ RemindMe/app/src/main/java/com/qqq/remindme/MainActivity.java
@@ -0,0 +1,15 @@
+package com.qqq.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by qqq on 4/1/2016.
+ */
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
